284ca9935a972b621bcc78a7b2a3ee85b85ca213
overtunned <gopinathabhishek@gmail.com>
2021-06-04 19:44:45 +0530

    update

--- lanedetectionusingcv.py
+++ lanedetectionusingcv.py
@@ -71,23 +71,3 @@ def lane_detect(img):
     return detected
 
 
-if __name__ == "__main__":
-    # image = cv2.imread('test_image.jpg')
-    # lane_image = np.copy(image)
-    # imposed = lane_detect(lane_image)
-    # cv2.imshow('image', imposed)
-    # cv2.waitKey(0)
-
-    cap = cv2.VideoCapture('test2.mp4')
-    while cap.isOpened():
-        _, frame = cap.read()
-        canny_image = canny(frame)
-        cropped = region_int(canny_image)
-        lines = cv2.HoughLinesP(cropped, 2, np.pi / 180, 100, np.array([]), minLineLength=10, maxLineGap=5)
-        averaged = averaged_slope_intercept(frame, lines)
-        line_image = disp_lines(frame, averaged)
-        detected = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
-        cv2.imshow('image', detected)
-        if cv2.waitKey(1) == ord('q'):
-            break
-
