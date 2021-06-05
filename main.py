f86f0693f7b712d6ea70585c14dbab4c99dfc6bc
overtunned <gopinathabhishek@gmail.com>
2021-06-05 22:54:23 +0530

    model and driver code

--- main.py
+++ main.py
@@ -1,22 +1,53 @@
+import socketio
+import eventlet
+import numpy as np
+from flask import Flask
+from keras.models import load_model
+import base64
+from io import BytesIO
+from PIL import Image
 import cv2
-from lanedetectionusingcv import *
 
-if __name__ == "__main__":
-    # image = cv2.imread('test_image.jpg')
-    # lane_image = np.copy(image)
-    # imposed = lane_detect(lane_image)
-    # cv2.imshow('image', imposed)
-    # cv2.waitKey(0)
+sio = socketio.Server()
 
-    cap = cv2.VideoCapture('test2.mp4')
-    while cap.isOpened():
-        _, frame = cap.read()
-        canny_image = canny(frame)
-        cropped = region_int(canny_image)
-        lines = cv2.HoughLinesP(cropped, 2, np.pi / 180, 100, np.array([]), minLineLength=10, maxLineGap=5)
-        averaged = averaged_slope_intercept(frame, lines)
-        line_image = disp_lines(frame, averaged)
-        detected = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
-        cv2.imshow('image', detected)
-        if cv2.waitKey(1) == ord('q'):
-            break
+app = Flask(__name__)
+speed_limit = 10
+def img_preprocess(img):
+    img = img[60:135,:,:]
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+    img = cv2.GaussianBlur(img,  (3, 3), 0)
+    img = cv2.resize(img, (200, 66))
+    img = img/255
+    return img
+
+
+@sio.on('telemetry')
+def telemetry(sid, data):
+    speed = float(data['speed'])
+    image = Image.open(BytesIO(base64.b64decode(data['image'])))
+    image = np.asarray(image)
+    image = img_preprocess(image)
+    image = np.array([image])
+    steering_angle = float(model.predict(image))
+    throttle = 1.0 - speed/speed_limit
+    print('{} {} {}'.format(steering_angle, throttle, speed))
+    send_control(steering_angle, throttle)
+
+
+
+@sio.on('connect')
+def connect(sid, environ):
+    print('Connected')
+    send_control(0, 0)
+
+def send_control(steering_angle, throttle):
+    sio.emit('steer', data = {
+        'steering_angle': steering_angle.__str__(),
+        'throttle': throttle.__str__()
+    })
+
+
+if __name__ == '__main__':
+    model = load_model('model.h5')
+    app = socketio.Middleware(sio, app)
+    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
